29dc3f225720ab908bd2c6cfbeb9f16533330b5e
prem-jeet <prem4300795@gmail.com>
2022-03-18 10:17:07 +0530

    added basic boiler plate

--- js/content.js
+++ js/content.js
@@ -3,6 +3,7 @@ let content = []
 content.push(require("./card_content/breakpoint_content"))
 content.push(require("./card_content/gridsystem_content"))
 content.push(require("./card_content/column_content"))
+content.push(require("./card_content/typography_content"))
 
 
 module.exports = content;
